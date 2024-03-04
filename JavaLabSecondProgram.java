1b713147947fa1c673030e78531215cee9bf8467
dasaradha583 <124852324+dasaradha583@users.noreply.github.com>
2024-03-04 17:21:52 +0530

    Update JavaLabSecondProgram.java

--- JavaLabSecondProgram.java
+++ JavaLabSecondProgram.java
@@ -9,24 +9,28 @@ public class JavaLabSecondProgram {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         Hashtable<String,String> hashtable= new Hashtable<String,String>();
+        Hashtable<String,String> hashtable1= new Hashtable<String,String>();
         FileInputStream fis = new FileInputStream("hello.txt");
         Scanner fileReader = new Scanner(fis).useDelimiter("    ");
         String dataArray[];
+        
         // String 
         while(fileReader.hasNext()){
             String str = fileReader.nextLine();
             dataArray = str.split("    ");
             // System.out.println(dataArray[1]);
             hashtable.put(dataArray[0],dataArray[1]);
+            hashtable1.put(dataArray[1],dataArray[0]);
         }
         String key;
-        System.out.print("Enter key ; ");
+        System.out.print("Enter key : ");
         key = scanner.nextLine();
-        System.out.println(hashtable);
         if(hashtable.containsKey(key)){
             System.out.println("value is : "+hashtable.get(key));
-        } else {
-            System.out.println("Given key is wrong");
+        }else if(hashtable1.containsKey(key)){
+            System.out.println("key is : "+hashtable1.get(key));
+        }else{
+            System.out.println("key or value not present");
         }
         fileReader.close();
         scanner.close();
